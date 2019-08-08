b804b7c7d5ad749e72bf77b8b1354a282d01ed39
Mez Gebre <mez@luminartech.com>
2019-08-08 12:55:33 -0700

    PIP reformat and more clean up

--- train.py
+++ train.py
@@ -14,12 +14,8 @@ from utils.graph import initialize_graph, update_graph, decode_tracks
 from utils.metrics import create_mot_accumulator, calc_mot_metrics
 from utils.training_options import args
 
-
 # This will set both cpu and gpu: https://pytorch.org/docs/stable/notes/randomness.html
 torch.manual_seed(args.seed)
-if args.cuda:
-    torch.cuda.manual_seed(args.seed)
-
 
 kwargs = {'batch_size': 1, 'shuffle': True, 'num_workers': 1}
 train_loader = DataLoader(KittiMOTSDataset(args.dataset_root_path, 'train', args.timesteps), **kwargs)
@@ -34,7 +30,7 @@ def train(model, epoch):
     epoch_loss = list()
     correct = 0.
     total = 0.
-    #motas = []
+    # motas = []
     model.train()
     for b_idx, (X, y) in enumerate(train_loader):
         if type(X) == type([]) or type(y) == type([]):
@@ -62,7 +58,8 @@ def train(model, epoch):
         # intialize graph and run first forward pass
         for t in range(t_init, t_end):
             # update graph for next timestep and run forward pass
-            y_pred, feats, node_adj, edge_adj, labels = update_graph(feats, node_adj, labels, scores, y_pred, X, y, t, mode='train', cuda=args.cuda)
+            y_pred, feats, node_adj, edge_adj, labels = update_graph(feats, node_adj, labels, scores, y_pred, X, y, t,
+                                                                     mode='train', cuda=args.cuda)
             scores = model.forward(feats, node_adj, edge_adj)
             # compute the loss
             loss += F.nll_loss(scores, labels)
@@ -71,10 +68,10 @@ def train(model, epoch):
             pred = scores.data.max(1)[1]  # get the index of the max log-probability
             correct += float(pred.eq(labels.data).cpu().sum())
             total += float(labels.size()[0])
-            #y_pred, feats, node_adj, scores = prune_graph(feats, node_adj, scores, y_pred, t1, t2, threshold=0.5)
-        #tracks = decode_tracks(node_adj, scores, y_pred)
-        #acc = create_mot_accumulator(tracks, y)
-        #if acc is not None:
+            # y_pred, feats, node_adj, scores = prune_graph(feats, node_adj, scores, y_pred, t1, t2, threshold=0.5)
+        # tracks = decode_tracks(node_adj, scores, y_pred)
+        # acc = create_mot_accumulator(tracks, y)
+        # if acc is not None:
         #    motas.append(calc_mot_metrics([acc])['mota'])
 
         epoch_loss.append(loss.item())
@@ -83,12 +80,12 @@ def train(model, epoch):
 
         if b_idx % args.log_schedule == 0:
             print('Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.5f}'.format(
-                epoch, (b_idx+1), len(train_loader.dataset),
-                100. * (b_idx+1) / len(train_loader.dataset), loss.item()))
+                epoch, (b_idx + 1), len(train_loader.dataset),
+                100. * (b_idx + 1) / len(train_loader.dataset), loss.item()))
             with open(os.path.join(args.output_dir, "logs.txt"), "a") as f:
                 f.write('Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.5f}\n'.format(
-                epoch, (b_idx+1), len(train_loader.dataset),
-                100. * (b_idx+1) / len(train_loader.dataset), loss.item()))
+                    epoch, (b_idx + 1), len(train_loader.dataset),
+                    100. * (b_idx + 1) / len(train_loader.dataset), loss.item()))
 
     # now that the epoch is completed calculate statistics and store logs
     avg_loss = statistics.mean(epoch_loss)
@@ -96,14 +93,13 @@ def train(model, epoch):
     with open(os.path.join(args.output_dir, "logs.txt"), "a") as f:
         f.write("\n------------------------\nAverage loss for epoch = {:.2f}\n".format(avg_loss))
 
-    
-    train_accuracy = 100.0*correct/total
-    #train_mota = 100.0*statistics.mean(motas)
+    train_accuracy = 100.0 * correct / total
+    # train_mota = 100.0*statistics.mean(motas)
     print("Accuracy for epoch = {:.2f}%\n------------------------".format(train_accuracy))
-    #print("MOTA for epoch = {:.2f}%\n------------------------".format(train_mota))
+    # print("MOTA for epoch = {:.2f}%\n------------------------".format(train_mota))
     with open(os.path.join(args.output_dir, "logs.txt"), "a") as f:
         f.write("Accuracy for epoch = {:.2f}%\n------------------------\n".format(train_accuracy))
-        #f.write("MOTA for epoch = {:.2f}%\n------------------------\n".format(train_mota))
+        # f.write("MOTA for epoch = {:.2f}%\n------------------------\n".format(train_mota))
 
     return model, avg_loss, train_accuracy, None
 
@@ -115,7 +111,7 @@ def val(model, epoch):
     total = 0.
     motas = []
     model.eval()
-    
+
     for b_idx, (X, y) in enumerate(val_loader):
         if type(X) == type([]) or type(y) == type([]):
             continue
@@ -137,24 +133,26 @@ def val(model, epoch):
         # intialize graph and run first forward pass
         for t in range(t_init, t_end):
             # update graph for next timestep and run forward pass
-            y_pred, feats, node_adj, edge_adj, labels = update_graph(feats, node_adj, labels, scores, y_pred, X, y, t, mode='train', cuda=args.cuda)
+            y_pred, feats, node_adj, edge_adj, labels = update_graph(feats, node_adj, labels, scores, y_pred, X, y, t,
+                                                                     mode='train', cuda=args.cuda)
             scores = model.forward(feats, node_adj, edge_adj)
             scores = torch.exp(scores)
             # compute the accuracy
             pred = scores.data.max(1)[1]  # get the index of the max log-probability
             correct += float(pred.eq(labels.data).cpu().sum())
             total += float(labels.size()[0])
-            #y_pred, feats, node_adj, scores = prune_graph(feats, node_adj, scores, y_pred, min(t-10, 0), t-1, threshold=0.5)
+            # y_pred, feats, node_adj, scores = prune_graph(feats, node_adj, scores, y_pred, min(t-10, 0), t-1, threshold=0.5)
         tracks = decode_tracks(node_adj, scores, y_pred)
         acc = create_mot_accumulator(tracks, y)
         if acc is not None:
             motas.append(calc_mot_metrics([acc])['mota'])
 
-        print('Done with sequence {} out {}...'.format(min(b_idx+1, len(val_loader.dataset)), len(val_loader.dataset)))
+        print(
+            'Done with sequence {} out {}...'.format(min(b_idx + 1, len(val_loader.dataset)), len(val_loader.dataset)))
 
     print("------------------------\nPredicted {} out of {}".format(correct, total))
-    val_accuracy = 100.0*correct/total
-    val_mota = 100.0*statistics.mean(motas)
+    val_accuracy = 100.0 * correct / total
+    val_mota = 100.0 * statistics.mean(motas)
     print("Validation accuracy = {:.2f}%".format(val_accuracy))
     print("Validation MOTA = {:.2f}%\n------------------------".format(val_mota))
     with open(os.path.join(args.output_dir, "logs.txt"), "a") as f:
@@ -173,10 +171,11 @@ def val(model, epoch):
 
 if __name__ == '__main__':
     # get the model, load pretrained weights, and convert it into cuda for if necessary
-    model = TrackMPNN(nfeat=1+4+64+10-10+64,
-            nhid=args.hidden,
-            nclass=2,
-            dropout=False)
+    model = TrackMPNN(nfeat=1 + 4 + 64 + 10 - 10 + 64,
+                      nhid=args.hidden,
+                      nclass=2,
+                      dropout=False)
+
     if args.snapshot is not None:
         model.load_state_dict(torch.load(args.snapshot), strict=False)
     if args.cuda:
@@ -188,7 +187,7 @@ if __name__ == '__main__':
     fig1, ax1 = plt.subplots()
     plt.grid(True)
     train_loss = list()
-    
+
     fig2, ax2 = plt.subplots()
     plt.grid(True)
     ax2.plot([], 'g', label='Train accuracy')
@@ -203,7 +202,7 @@ if __name__ == '__main__':
 
     train_acc, val_acc, train_mota, val_mota = list(), list(), list(), list()
 
-    for i in range(1, args.epochs+1):
+    for i in range(1, args.epochs + 1):
         model, avg_loss, acc, mota = train(model, i)
         train_acc.append(acc)
         train_mota.append(mota)
@@ -221,7 +220,7 @@ if __name__ == '__main__':
         ax2.plot(train_acc, 'g', label='Train accuracy')
         ax2.plot(val_acc, 'b', label='Validation accuracy')
         fig2.savefig(os.path.join(args.output_dir, 'train_val_accuracy.jpg'))
-        
+
         ax3.plot(train_mota, 'g', label='Train MOTA')
         ax3.plot(val_mota, 'b', label='Validation MOTA')
         fig3.savefig(os.path.join(args.output_dir, 'train_val_mota.jpg'))
