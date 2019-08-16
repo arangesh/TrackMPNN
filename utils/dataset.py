934b2013d205c45f5ae8292ee682498d555b49d1
Akshay Rangesh <arangesh@ucsd.edu>
2019-08-16 08:53:47 -0700

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -15,8 +15,12 @@ def get_tracking_data(dataset_path, split, timesteps):
 
     # Load tracking dataset; each row is [seq_no, st_fr, ed_fr]
     dataset = []
-    for i, seq in enumerate(seqs):
-        for st_fr in range(0, num_frames[i], int(timesteps / 2)):
-            dataset.append([seq, st_fr, min(st_fr + timesteps, num_frames[i])])
+    if split == 'val':
+        for i, seq in enumerate(seqs):
+            dataset.append([seq, 0, num_frames[i]])
+    else:
+        for i, seq in enumerate(seqs):
+            for st_fr in range(0, num_frames[i], int(timesteps / 2)):
+                dataset.append([seq, st_fr, min(st_fr + timesteps, num_frames[i])])
 
     return dataset
