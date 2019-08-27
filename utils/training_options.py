c9916a649278408e8884ddec7e6f7889ab881f42
Akshay Rangesh <arangesh@ucsd.edu>
2019-08-26 19:37:48 -0700

    Update training_options.py

--- utils/training_options.py
+++ utils/training_options.py
@@ -10,6 +10,8 @@ parser.add_argument('--dataset-root-path', type=str, default='/home/akshay/data/
 parser.add_argument('--output-dir', type=str, default=None, help='output directory for model and logs')
 parser.add_argument('--snapshot', type=str, default=None, help='use a pre-trained model snapshot')
 parser.add_argument('--timesteps', type=int, default=5, metavar='TS', help='number of timesteps to train on')
+parser.add_argument('--greedy', action='store_true', default=False, help='decode tracks in a greedy manner (if not activated, use Hungarian)')
+parser.add_argument('--no-tp-classifier', action='store_true', default=False, help='train network to only classify edges')
 parser.add_argument('--hidden', type=int, default=64, metavar='NH', help='number of hidden layer nodes')
 parser.add_argument('--epochs', type=int, default=100, metavar='EP', help='number of epochs to train for')
 parser.add_argument('--learning-rate', type=float, default=0.0001, metavar='LR', help='learning rate')
@@ -17,7 +19,6 @@ parser.add_argument('--momentum', type=float, default=0.9, metavar='M', help='mo
 parser.add_argument('--weight-decay', type=float, default=0.0005, metavar='WD', help='weight decay')
 parser.add_argument('--log-schedule', type=int, default=10, metavar='N', help='number of iterations to print/save log after')
 parser.add_argument('--seed', type=int, default=1, help='set seed to some constant value to reproduce experiments')
-parser.add_argument('--no-tp-classifier', action='store_true', default=False, help='train network to only classify edges')
 parser.add_argument('--no-cuda', action='store_true', default=False, help='do not use cuda for training')
 
 
@@ -25,6 +26,7 @@ args = parser.parse_args()
 
 # setup args
 args.tp_classifier = not args.no_tp_classifier
+args.hungarian = not args.greedy
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 if args.output_dir is None:
     args.output_dir = datetime.now().strftime("%Y-%m-%d-%H:%M")
