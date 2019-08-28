2eb13c05eec9221f3f43ca19d1dd08e0b613d52a
Akshay Rangesh <arangesh@ucsd.edu>
2019-08-28 09:58:00 -0700

    Update training_options.py
    Make default option greedy instead of Hungarian as this seems to work better anyway.

--- utils/training_options.py
+++ utils/training_options.py
@@ -10,7 +10,7 @@ parser.add_argument('--dataset-root-path', type=str, default='/home/akshay/data/
 parser.add_argument('--output-dir', type=str, default=None, help='output directory for model and logs')
 parser.add_argument('--snapshot', type=str, default=None, help='use a pre-trained model snapshot')
 parser.add_argument('--timesteps', type=int, default=5, metavar='TS', help='number of timesteps to train on')
-parser.add_argument('--greedy', action='store_true', default=False, help='decode tracks in a greedy manner (if not activated, use Hungarian)')
+parser.add_argument('--hungarian', action='store_true', default=False, help='decode tracks using frame-by-frame Hungarian algorithm')
 parser.add_argument('--no-tp-classifier', action='store_true', default=False, help='train network to only classify edges')
 parser.add_argument('--hidden', type=int, default=64, metavar='NH', help='number of hidden layer nodes')
 parser.add_argument('--epochs', type=int, default=100, metavar='EP', help='number of epochs to train for')
@@ -26,7 +26,6 @@ args = parser.parse_args()
 
 # setup args
 args.tp_classifier = not args.no_tp_classifier
-args.hungarian = not args.greedy
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 if args.output_dir is None:
     args.output_dir = datetime.now().strftime("%Y-%m-%d-%H:%M")
