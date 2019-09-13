8da407395d26dde69e01bf415d375d79727b8102
Akshay Rangesh <arangesh@ucsd.edu>
2019-09-13 13:14:25 -0700

    Update track_mpnn.py
    Added batch norm for initial hidden state embedding.

--- models/track_mpnn.py
+++ models/track_mpnn.py
@@ -16,7 +16,7 @@ class TrackMPNN(nn.Module):
         self.input_transform_2 = nn.Linear(nhid, nhid, bias=True)
         self.input_transform_2.weight.data.normal_(mean=0.0, std=0.01)
         self.input_transform_2.bias.data.uniform_(0, 0)
-        self.input_transform = nn.Sequential(self.input_transform_1, nn.ReLU(), self.input_transform_2)
+        self.input_transform = nn.Sequential(self.input_transform_1, nn.BatchNorm1d(nhid), nn.ReLU(), self.input_transform_2)
 
         self.factor_gru1 = FactorGraphGRU(nhid, bias=True, msg_type='concat')
 
@@ -40,6 +40,7 @@ class TrackMPNN(nn.Module):
                 conv_hull = torch.cat((conv_hull.view(-1, 2, 5), torch.zeros(conv_hull.size()[0], 1, 5).cuda()), dim=1) # (N'-N, 3, 5)
             else:
                 conv_hull = torch.cat((conv_hull.view(-1, 2, 5), torch.zeros(conv_hull.size()[0], 1, 5)), dim=1) # (N'-N, 3, 5)
+
             # batchnorm does not work if N=1 (workaround)
             if conv_hull.size()[0] == 1:
                 conv_hull_feat, _, _ = self.pointnet(torch.cat((conv_hull, conv_hull), 0)) # (2, 64)
@@ -48,15 +49,22 @@ class TrackMPNN(nn.Module):
                 conv_hull_feat, _, _ = self.pointnet(conv_hull) # (N'-N, 64)
                 x = torch.cat((x[:, :-10], conv_hull_feat), dim=1) # (N'-N, F-10+64)
 
-            h_update = torch.mm(I_node.to_dense()[-x.size()[0]:, -x.size()[0]:].to_sparse(), self.input_transform(x)) # (N'-N, 64)
-            if h_in is None: # (N, 64)
-                h = h_update # (N', 64)
+            # batchnorm does not work if N=1 (workaround)
+            if x.size()[0] == 1:
+                x = self.input_transform(torch.cat((x, x), 0)) # (2, nhid)
+                x = x[:1, :] # (1, nhid)
             else:
-                h = torch.cat((h_in, h_update), dim=0) # (N', 64)
+                x = self.input_transform(x) # (N'-N, nhid)
+
+            h_update = torch.mm(I_node.to_dense()[-x.size()[0]:, -x.size()[0]:].to_sparse(), x) # (N'-N, nhid)
+            if h_in is None: # (N, nhid)
+                h = h_update # (N', nhid)
+            else:
+                h = torch.cat((h_in, h_update), dim=0) # (N', nhid)
         else:
             h = h_in
 
-        h_out = self.factor_gru1(h, node_adj, edge_adj) # (N', 64)
+        h_out = self.factor_gru1(h, node_adj, edge_adj) # (N', nhid)
         y = torch.mm(I_node, self.output_transform_node(h_out)) + torch.mm(I_edge, self.output_transform_edge(h_out)) # (N', 1)
 
         return self.output_activation(y), h_out
