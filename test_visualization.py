f43c8dba2123b6f2368fb4a8a5d8756f90361977
lekhamohan <lekha.mohan@luminartech.com>
2019-09-19 08:31:24 -0700

    clean up

--- test_visualization.py
+++ test_visualization.py
@@ -6,14 +6,6 @@ import random
 from IPython import embed
 
 if __name__ == '__main__':
-    # Generate random array vals for frame_id and track_id
-    frame_id = np.random.randint(7, size=30)
-    track_id = np.random.randint(15, size=30)
-
-    # Fake Data and called the graph visualization function
-    # y_out = np.vstack((frame_id, track_id)).T
-    # y_in = y_out + 1 #Equal for the time being
-    # y_in[:,0] =y_out[:,0] #Setting the frame no.s to be equal in y_in and y_out
 
     #Load from saved np.array
     y_in = np.loadtxt('Y/y_seq_2.txt', dtype=int)
