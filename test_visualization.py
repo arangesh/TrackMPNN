c89787a4ac090b113a48d8eb89450fc26bb63066
lekhamohan <lekha.mohan@luminartech.com>
2019-09-20 16:43:58 -0700

    formatting, clean up

--- test_visualization.py
+++ test_visualization.py
@@ -9,15 +9,15 @@ if __name__ == '__main__':
     # Load from saved np.array
     y_in = np.loadtxt('sample_data/y_seq_2.txt', dtype=int)
     y_out = np.loadtxt('sample_data/y_out_seq_2.txt', dtype=int)
+    ITER = 2 #Num batches for running the visualizer
 
     # TODO IDEALLY RESOLUTION COULD BE TIMESTEPS
-    resolution = int(len(y_out) / 2)
+    resolution = int(len(y_out) / ITER)
     node_name_container = np.arange(len(y_in))  # Retains the names of nodes over iterations of graph generation
-    embed()
-    # You will thank me later
+
     batch = 0
-    color_label_dict = {}
-    for i in range(2):
+    color_label_dict = {} # You will thank me later for passing rhis in
+    for num in range(ITER):
         color_label_dict = generate_dynamic_graph(y_in[batch: batch + resolution, :],
                                                   y_out[batch: batch + resolution, :],
                                                   color_label_dict, node_name_container[batch: batch + resolution])
