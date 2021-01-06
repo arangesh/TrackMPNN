Fixed some minor bugs in inference + mot metrics calculation

infer.py
@@ -55,7 +55,8 @@ def infer(model):
             # update graph for next timestep and run forward pass
             y_pred, feats, node_adj, edge_adj, labels = update_graph(node_adj, labels, scores, y_pred, X_seq, y_seq, t_cur, 
                 use_hungraian=args.hungarian, mode='test', cuda=args.cuda)
-            if feats.size()[0] == 0:
+            # if no new detections found and no carried over detections, continue
+            if feats.size()[0] == 0 and states.size()[0] == 0:
                 print("Sequence {}, generated tracks upto t = {}/{}...".format(b_idx + 1, max(0, t_cur - args.cur_win_size + 1), t_end))
                 continue
             scores, logits, states = model(feats, states, node_adj, edge_adj)
@@ -64,10 +65,6 @@ def infer(model):
                 idx_node = torch.nonzero((y_pred[:, 0] != -1))[:, 0]
                 scores[idx_node, 0] = 0
                 scores[idx_node, 1] = 1
-
-            # if no new detections are added, don't remove detections either
-            if feats.size()[0] == 0:
-                continue
 
             if t_cur == t_end - 1:
                 y_pred, y_out, states, node_adj, labels, scores = decode_tracks(states, node_adj, labels, scores, y_pred, y_out, t_end, 

train.py
@@ -85,7 +85,8 @@ def train(model, epoch):
             # update graph for next timestep and run forward pass
             y_pred, feats, node_adj, edge_adj, labels = update_graph(node_adj, labels, scores, y_pred, X_seq, y_seq, t_cur, 
                 use_hungraian=args.hungarian, mode='train', cuda=args.cuda)
-            if feats.size()[0] == 0:
+            # if no new detections found and no carried over detections, continue
+            if feats.size()[0] == 0 and states.size()[0] == 0:
                 continue
             scores, logits, states = model(feats, states, node_adj, edge_adj)
             # compute the loss
@@ -205,6 +206,10 @@ def val(model, epoch):
             # update graph for next timestep and run forward pass
             y_pred, feats, node_adj, edge_adj, labels = update_graph(node_adj, labels, scores, y_pred, X_seq, y_seq, t_cur, 
                 use_hungraian=args.hungarian, mode='test', cuda=args.cuda)
+            # if no new detections found and no carried over detections, continue
+            if feats.size()[0] == 0 and states.size()[0] == 0:
+                print("Sequence {}, generated tracks upto t = {}/{}...".format(b_idx + 1, max(0, t_cur - args.cur_win_size + 1), t_end))
+                continue
             scores, logits, states = model(feats, states, node_adj, edge_adj)
             scores = torch.cat((1-scores, scores), dim=1)
 
@@ -221,10 +226,6 @@ def val(model, epoch):
             # compute the f1 score
             pred = scores.data.max(1)[1]  # get the index of the max log-probability
             epoch_f1.append(f1_score(targets[idx].detach().cpu().numpy(), pred[idx].detach().cpu().numpy()))
-
-            # if no new detections are added, don't remove detections either
-            if feats.size()[0] == 0:
-                continue
 
             if t_cur == t_end - 1:
                 y_pred, y_out, states, node_adj, labels, scores = decode_tracks(states, node_adj, labels, scores, y_pred, y_out, t_end, 
@@ -335,6 +336,7 @@ if __name__ == '__main__':
     ax3.legend()
 
     train_f1, val_f1, val_mota, val_map  = list(), list(), list(), list()
+    f1, mota, mAP = val(model, 0)
 
     for i in range(1, args.epochs + 1):
         model, avg_loss_d, avg_loss_c, avg_loss_f, avg_loss, avg_f1 = train(model, i)

utils/metrics.py
@@ -16,11 +16,10 @@ def create_mot_accumulator(bbox_pred, bbox_gt, y_out, y_gt):
     Returns: 
     A MOT accumulator object
     """
-    if np.all(y_gt[:, 1] == -1) or np.all(y_out[:, 1] == -1):
-        return None
-    times = np.sort(y_gt[:, 0])
-    t_st = times[0]
-    t_ed = times[-1]
+    times_out = np.sort(y_out[:, 0])
+    times_gt = np.sort(y_gt[:, 0])
+    t_st = min(times_gt[0], times_out[0])
+    t_ed = max(times_gt[-1], times_out[-1])
 
     # initialize and load tracking results into MOT accumulator
     acc = mm.MOTAccumulator()